756299f305c5df5f8597fc2dfd700c4568283ac7
Romain BOURGET <acuity18@gmail.com>
2025-02-25 15:40:30 +0100

    added gitignore and .env

--- bg_changing/main.py
+++ bg_changing/main.py
@@ -1,24 +1,28 @@
 from PIL import Image
 from random import randint
 import ctypes
-from progress.bar import IncrementalBar
 import time
 import keyboard as k
+from dotenv import load_dotenv
+from os import getenv
 
+load_dotenv(".env")
 
-path = "C:/Users/Romain/Documents/code/python/dataInImage/bg_changing/exe/dist/main"
+path = getenv("BG_PATH")
+
 
 def generate():
-    sizeex = randint(1,3)
-    sizeey = randint(1,3)
-    sizee = (sizeex,sizeey)
-    im = Image.new(mode="RGBA",size=sizee)
+    sizeex = randint(1, 3)
+    sizeey = randint(1, 3)
+    sizee = (sizeex, sizeey)
+    im = Image.new(mode="RGBA", size=sizee)
     pixels = im.load()
     for i in range(im.size[0]):
         for j in range(im.size[1]):
-            pixels[i,j] = (randint(0,255),randint(0,255),randint(0,255))
+            pixels[i, j] = (randint(0, 255), randint(0, 255), randint(0, 255))
     im.save(f"{path}/bg.png")
-    ctypes.windll.user32.SystemParametersInfoW(20, 0,f"{path}/bg.png", 0)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{path}/bg.png", 0)
+
 
 while not k.is_pressed("1"):
     time.sleep(10)
