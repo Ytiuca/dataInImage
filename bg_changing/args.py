78b7b67d8beabec503a54928199ada0d5d117edd
Romain BOURGET <acuity18@gmail.com>
2025-03-05 09:40:53 +0100

    removed Logger & time_this_function because its personnal lib

--- bg_changing/args.py
+++ bg_changing/args.py
@@ -1,7 +1,4 @@
 import argparse
-from logger import Logger
-
-LOGGER = Logger()
 
 parser = argparse.ArgumentParser("bg_changing")
 parser.add_argument("-x", "--x_length", type=int)
@@ -10,4 +7,3 @@ parser.add_argument("-d", "--delay", type=int)
 parser.add_argument("-s", "--save", action=argparse.BooleanOptionalAction)
 
 arguments = parser.parse_args()
-LOGGER.debug(f"{arguments.save=}")
